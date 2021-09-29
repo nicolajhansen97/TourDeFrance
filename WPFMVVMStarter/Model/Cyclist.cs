b8c980521855047682fffb429ae4b6303a8f54f0
Nicolaj Hansen <nicolajhansen97@live.dk>
2021-09-29 12:57:28 +0200

    Cyclist made (QUESTION A)

---
+++ WPFMVVMStarter/Model/Cyclist.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMVVMStarter.Model
+{
+    class Cyclist
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string gender;
+
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        private string countryOrigin;
+
+        public string CountryOrigin
+        {
+            get { return countryOrigin; }
+            set { countryOrigin = value; }
+        }
+
+        private string resultTime;
+
+        public string ResultTime
+        {
+            get { return resultTime; }
+            set { resultTime = value; }
+        }
+
+        private int endPosition;
+
+        public int EndPosition
+        {
+            get { return endPosition; }
+            set { endPosition = value; }
+        }
+
+
+    }
+}
